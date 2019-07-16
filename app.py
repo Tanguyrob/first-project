953cdab7f9215ba1c2146a204f0c315c6a579a35
Tanguy 2 <Tanguy Robilliard>
2019-07-16 16:29:24 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello2():
+    return render_template("home.html", message = "Hello 2")
+
+@app.route("/a")
+def fonction_suivante():
+    return render_template("welcome.html")
+    
+
+@app.route("/suite", methods= ['GET' , 'POST'])
+def page3():
+    #if request.method == 'POST':
+    text = request.form.get("text")
+    print(text)
+    processed_text = text.upper()
+    return render_template("page_suivante.html", message = processed_text)
+    '''
+    else:
+        return "methode get" 
+    '''
+    '''       
+    text = request.form['text']
+    processed_text = text.upper()
+    return render_template("page_suivante.html", message = processed_text)
+    '''
+
+@app.route("/fin")
+def last_function():
+    return render_template("home.html", message = "vous êtes de retour sur la page 1".upper())
+
+
+if __name__ == '__main__':
+    app.run()
